13eb5cbe2ab36e9d08686365f7179d2885d87c9d
Kitaozao <kito_and@hotmail.com>
2017-02-23 22:17:07 -0300

    inserir, editar e apagar funcionando em nova lista

--- MyIonic2Project/src/pages/addlist/addlist.ts
+++ MyIonic2Project/src/pages/addlist/addlist.ts
@@ -1,22 +1,74 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/*
-  Generated class for the Addlist page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+import { NavController, AlertController } from 'ionic-angular';
+ 
 @Component({
   selector: 'page-addlist',
   templateUrl: 'addlist.html'
 })
 export class AddlistPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddlistPage');
-  }
-
+ 
+    itens: any = [];
+ 
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+ 
+    }
+ 
+    AddIten(){
+ 
+        let prompt = this.alertCtrl.create({
+            title: 'Adicionar Item',
+            inputs: [{ type: 'string',placeholder: 'Produto', name: 'title'},
+            {type: 'number',placeholder: 'Quantidade', name: 'quantidade'}],
+            buttons: [
+                {
+                    text: 'Cancelar'
+                },
+                {
+                    text: 'Adicionar',
+                    handler: data => {
+                        this.itens.push(data);
+                    }
+                }
+            ]
+        });
+ 
+        prompt.present();
+    }
+ 
+    editIten(iten){
+ 
+        let prompt = this.alertCtrl.create({
+            title: 'Editar Produto',
+            inputs: [{ type: 'string',placeholder: 'Produto', name: 'title'},
+            {type: 'number',placeholder: 'Quantidade', name: 'quantidade'}],
+            buttons: [
+                {
+                    text: 'Cancelar'
+                },
+                {
+                    text: 'Salvar',
+                    handler: data => {
+                        let index = this.itens.indexOf(iten);
+ 
+                        if(index > -1){
+                          this.itens[index] = data;
+                        }
+                    }
+                }
+            ]
+        });
+ 
+        prompt.present();       
+ 
+    }
+ 
+    deleteIten(iten){
+ 
+        let index = this.itens.indexOf(iten);
+ 
+        if(index > -1){
+            this.itens.splice(index, 1);
+        }
+    }
+ 
 }
